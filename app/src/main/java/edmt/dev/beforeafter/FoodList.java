777f696d1c9b91f603e31a60e1a3b15207922e0b
Aries <lactinphong123@gmail.com>
2018-04-17 21:21:54 +0700

    update 21h21 17/4/2018

--- app/src/main/java/edmt/dev/beforeafter/FoodList.java
+++ app/src/main/java/edmt/dev/beforeafter/FoodList.java
@@ -1,5 +1,6 @@
 package edmt.dev.beforeafter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -75,6 +76,11 @@ public class FoodList extends AppCompatActivity {
                     @Override
                     public void onClick(View view, int position, boolean isLongClick) {
                         Toast.makeText(FoodList.this, ""+local.getName(), Toast.LENGTH_SHORT).show();
+
+                        // Star new act
+                        Intent foodDetail = new Intent(FoodList.this,FoodDetail.class);
+                        foodDetail.putExtra("FoodId",adapter.getRef(position).getKey());
+                        startActivity(foodDetail);
                     }
                 });
 
